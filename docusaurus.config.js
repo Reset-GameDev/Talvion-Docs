5a4c2b427a6da0cef03f10c59e494ec3b79c51f4
TheMultii <info@mganczarczyk.pl>
2023-02-01 15:11:51 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'Talvion',
+            docId: 'Talvion/Talvion',
             position: 'left',
             label: 'Docs',
           },
